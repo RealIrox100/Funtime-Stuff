6ea9c374f342e919a7fd838251c0962fbee616c7
RealIrox100 <superstarfart02@gmail.com>
2026-09-24 15:59:09 -0400

    Fix indentation and formatting in dirlister.py

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -1,5 +1,7 @@
-import os 
+
+import os
+
 def run(**args):
-print("[*] In dirlister module")
-files = os.listdir(".")
-return str(files)
+    print("[*] In dirlister module")
+    files = os.listdir(".")
+    return str(files)
