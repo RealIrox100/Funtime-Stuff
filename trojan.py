a9f0142d396c3095d567f898ca895961b88a033b
RealIrox100 <superstarfart02@gmail.com>
2026-09-24 16:59:25 -0400

    Fix instantiation of Trojan class in main block

--- trojan.py
+++ trojan.py
@@ -92,5 +92,5 @@ if __name__ == '__main__':
     # Add the GitImporter to the system's module search path
     sys.meta_path.append(GitImporter())
     # Create a Trojan instance with a specific ID and run it
-    trojan = Trojan('abc')
+    trojan = trojan('abc')
     trojan.run()
